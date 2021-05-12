2269e914f130c8280c2bb30c63ae5145d297b39a
Patrick Lee <patricklee1138@live.com>
2021-05-12 14:49:51 -0700

    :construction: WIP: add UI
    Co-authored-by: Isaac Moreno <ipdxcreative@gmail.com>
    Co-authored-by: Jamie Knutsen <knutsenjamie@yahoo.com>

--- Tamagotchi/Controllers/HomeController.cs
+++ Tamagotchi/Controllers/HomeController.cs
@@ -1,13 +1,32 @@
 using Microsoft.AspNetCore.Mvc;
 using Tamagotchi.Models;
+using System.Collections.Generic;
 
 namespace Tamagotchi.Controllers
 {
   public class HomeController : Controller
   {
 
-    [Route("/")]
-    public ActionResult Title() { return View(); }
+    [HttpGet("/")]
+    public ActionResult Index() { return View(); }
+    // {
+    //   List<Pet> allPets = Pet.GetAll();
+    //   return View(allPets);
+    // }
+    [HttpPost("/game")]
+    {
+      public ActionResult Index() { return View(); }
 
+      public ActionResult Create(string name, string food, int attention, int time)
+      {
+        Pet TamagotchiInfo = new(
+          name,
+          food,
+          Convert.ToInt32(attention),
+          Convert.ToInt32(time)
+        );
+        return View(TamagotchiInfo);     
+      }
+    }
   }
 }
